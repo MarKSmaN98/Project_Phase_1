c913d250db952ed74e19f52874af3daf4e1168e0
Mark Coats <alexco1234@gmail.com>
2023-02-13 09:56:12 -0600

    markBranch first commit

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Project_Phase_1
+
+collab:
+Bryn Morris
+Jesse Hunter
+Mark Coats
---
+++ index.js
@@ -0,0 +1,30 @@
+const finalArr = new Map();
+let url = 'https://api.weather.gov/gridpoints/LWX/30,77/forecast/hourly';
+let iterator = fetch(url);
+iterator
+    .then(response => {
+        return response.json();
+    })
+    .then(x => {
+        const dataHead = x;
+        const tempArr = dataHead.properties.periods;
+
+        let iter, time, temp;
+        iter = 0;
+
+        for(element in tempArr) {
+            //console.log('iterator = ' + iter);
+            time = tempArr[iter].startTime;
+            time = time.slice(11,16);
+            temp = tempArr[iter].temperature + 'F';
+            finalArr.set(time, temp);
+            ++iter;
+        }
+
+        
+        
+        
+        //console.log(time);
+        //console.log(temp);
+        console.log(finalArr);
+    });
